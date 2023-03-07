ca834e5b7eecd7121996b82c1e466abd5a3d0beb
Illusion 705 <65733710+Illusion705@users.noreply.github.com>
2023-03-07 00:54:55 +0000

    Create home page

---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+$("header").load("/public/components/header.html")
--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,9 @@
 const router = require("express").Router();
 
 // routes
+router.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
 // export router
 module.exports = router;
