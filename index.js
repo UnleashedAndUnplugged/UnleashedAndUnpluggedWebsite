4bd504f95552107e3872fc68e6b9e344e3211078
Illusion 705 <65733710+Illusion705@users.noreply.github.com>
2023-03-07 00:29:03 +0000

    Setup express app and database

---
+++ index.js
@@ -0,0 +1,35 @@
+// dependencies
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+// create app
+const app = express();
+
+// app setup
+app.set("view-engine", "ejs");
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routers
+const mainRouter = require("./routes/main");
+const apiRouter = require("./routes/api");
+
+app.use("/", mainRouter);
+app.use("/api", apiRouter);
+
+// database setup
+const port = process.env.PORT || 3001;
+const dbString = process.env.DB_STRING;
+
+console.log("Connecting to database...");
+
+(async function connectDatabaseAndRun() {
+  await mongoose.connect(dbString);
+  console.log("Connected to database.");
+  
+  // init app
+  app.listen(port, () => console.log(`Server listening on port ${port}.`));
+})();
---
+++ routes/api.js
@@ -0,0 +1,7 @@
+// dependencies
+const router = require("express").Router();
+
+// routes
+
+// export router
+module.exports = router;
